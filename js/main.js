ef76f6f34824148043dac6fc0ec9a41c3f534bf8
Yurii Rybachok <drummer2009@ukr.net>
2023-06-13 10:28:52 +0300

    mask for input with phone number

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ navBtn.onclick = function () {
     nav.classList.toggle('header__top-row--mobile');
     document.body.classList.toggle('no-scroll'); 
 }
+
+/* Phone Mask */
+mask('[data-tel-input]');
+
+// Delete '+' in input phone
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input) => {
+    input.addEventListener('input', () => {
+        if (input.value == '+') input.value = '';
+    })
+    input.addEventListener('blur', () => {
+        if (input.value == '+') input.value = '';
+    })
+});
